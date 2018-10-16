1aceccb3f825680ac7f717c2f71c984974142c00
lovit <soy.lovit@gmail.com>
2018-10-17 02:22:57 +0900

    Clarify func names

--- better_topic_modeling/relevance.py
+++ better_topic_modeling/relevance.py
@@ -4,7 +4,7 @@ import scipy
 def topic_relevance_score(topic_term_dist, doc_topic_dist):
     raise NotImplemented
 
-def uniform_over_words(topic_term_prob):
+def uniform_topic_over_words(topic_term_prob):
     """Kullback-Leibler divergence between P(w|t) and 1/len(w)"""
 
     n_topics, n_terms = topic_term_prob.shape
@@ -14,7 +14,7 @@ def uniform_over_words(topic_term_prob):
         kl[t] = scipy.stats.entropy(topic_term_prob[t], uniform)
     return kl
 
-def topic_weighted_uniform_over_words(topic_term_prob, topic_prob):
+def weighted_uniform_topic_over_words(topic_term_prob, topic_prob):
     n_topics, n_terms = topic_term_prob.shape
     uniform = np.dot(topic_prob, topic_term_prob)
     kl = np.zeros(n_topics)
@@ -22,7 +22,7 @@ def topic_weighted_uniform_over_words(topic_term_prob, topic_prob):
         kl[t] = scipy.stats.entropy(topic_term_prob[t], uniform)
     return kl
 
-def uniform_over_docs(doc_topic_prob):
+def uniform_topic_over_docs(doc_topic_prob):
     n_docs, n_topics = doc_topic_prob.shape
     uniform = np.ones(n_docs) / n_docs
     kl = np.zeros(n_topics)
