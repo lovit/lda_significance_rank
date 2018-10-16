59edd0f5f1c8d06712e0694bf18873cd492388b5
lovit <soy.lovit@gmail.com>
2018-10-16 22:39:03 +0900

    Commit w uniform

--- better_topic_modeling/relevance.py
+++ better_topic_modeling/relevance.py
@@ -1,2 +1,15 @@
+import numpy as np
+import scipy
+
 def topic_relevance_score(topic_term_dist, doc_topic_dist):
     raise NotImplemented
+
+def w_uniform(topic_term_prob):
+    """Kullback-Leibler divergence between P(w|t) and 1/len(w)"""
+
+    n_topics, n_terms = topic_term_prob.shape
+    u_prob = np.ones(n_terms) / n_terms
+    kl_w = np.zeros(n_topics)
+    for t in range(n_topics):
+        kl_w[t] = scipy.stats.entropy(topic_term_prob[t], u_prob)
+    return kl_w
